e2b8b43a4d5b9643d3e93aff4727417c45233a3d
Faizaan Saifi <faizaansaifi331@yahoo.com>
2019-04-03 00:26:47 +0530

    To-do Task Manager. Username: alpha, Password:7788

---
+++ to-do/src/Component/Login.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react';
+import { withRouter } from 'react-router';
+
+class Login extends Component{
+    state=  {
+        user:"alpha",
+        pass:'7788',
+        temp_u: "",
+        temp_p:"",
+        valid:0
+    }
+
+    onHandleChange=(event)=>{
+        this.setState({
+            temp_u:event.target.value
+
+        });
+    };
+    passInput=(event)=>{
+        this.setState({
+            temp_p:event.target.value
+        })
+    }
+
+
+    onLogin=(event)=>{
+        event.preventDefault();
+        const { user, temp_u,pass,temp_p} = this.state;
+
+        const u_matches = user === temp_u;
+
+        const p_matches= pass===temp_p;
+
+        u_matches && p_matches?this.setState({valid:true}):alert("Username or Password may be Incorrect");
+        this.props.history.push('/todo')
+
+        console.log(this.props);
+    }
+
+    render() {
+        return(
+            <div>
+                <div>
+                    Login Here
+                </div>
+                <form onSubmit={(event)=>this.onLogin(event)} className={'loginForm'}>
+                    <input type={'text'}
+                           placeholder={"Username"}
+                           name={"username"}
+                           onChange={this.onHandleChange}/>
+                    <input type={'password'}
+                           placeholder={'Password'}
+                           name={"password"}
+                           onChange={this.passInput}/>
+                    <button type={'submit'}>Login</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Login);
